31ea4486f0dce12749a06941dd23f7fa829a6ba3
Orel <ryiseld@gmail.com>
2021-02-03 21:27:05 +0200

    Add id to messages, scroll to bottom on update, styling

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,129 +1,170 @@
 <template>
   <div>
-		<div class="notification is-danger" v-if="error">
-			{{ error }}
-		</div>
+    <div class="notification is-danger" v-if="error">
+      {{ error }}
+    </div>
 
-		<div v-if="!error">
-			<div class="columns">
-				<div class="column">
-					<h4 class="title is-4">Welcome to chat room: {{ code }}</h4>
-				</div>
-				<div class="column" style="text-align: right">
-					<a class="button is-danger" @click="deleteRoom()">Delete Room</a>
-				</div>
-			</div>
+    <div v-if="!error">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <h4 class="title is-4">Welcome to chat room: {{ code }}</h4>
+          </div>
+          <div class="column" style="text-align: right">
+            <a class="button is-danger" @click="deleteRoom()">Delete Room</a>
+          </div>
+        </div>
 
-			<div class="columns chatbox">
-				<div class="column">
-					<div class="messages">
-						<div v-for="message in messages"
-							v-bind:key="message.text" class="bubble" :class="message.sender != userId ? 'received' : 'sent'">
-							{{ message.text }}
-						</div>
-					</div>
+        <div class="columns chatbox">
+          <div class="column">
+            <div class="messages" id="messagesBox">
+              <p
+                v-for="message in messages"
+                v-bind:key="message.id"
+                class="bubble"
+                :class="message.sender != userId ? 'received' : 'sent'"
+              >
+                {{ message.text }}
+              </p>
+              <div id="anchor"></div>
+            </div>
 
-					<form class="field has-addons" v-on:submit.prevent="sendMessage()">
-						<div class="control is-expanded">
-							<input type="text" class="input" v-model="textInput" placeholder="Say something...">
-						</div>
-						<div class="control">
-							<button type="submit" class="button is-primary">Send</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
+            <form class="field has-addons" v-on:submit.prevent="sendMessage()">
+              <div class="control is-expanded">
+                <input
+                  type="text"
+                  class="input"
+                  v-model="textInput"
+                  placeholder="Say something..."
+                />
+              </div>
+              <div class="control">
+                <button type="submit" class="button is-primary">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 import router from '@/router';
-import store from '@/store';
 import firebase from '@/firebase';
 import db from '@/db';
 
 export default {
   name: 'Chat',
   data() {
-		return {
-			error: null,
-			code: '',
-			messages: [],
-			textInput: ''
-		}
-	},
+    return {
+      error: null,
+      code: '',
+      messages: [],
+      textInput: '',
+    };
+  },
 
-	computed: mapState(['userId']),
+  computed: mapState(['userId']),
 
-	async created() {
-		// Check if not logged in
-		if (this.userId == '') {
-			router.push('/');
-		}
+  async created() {
+    // Check if not logged in
+    if (this.userId == '') {
+      router.push('/');
+    }
 
-		// TODO: uncomment this validation
-		// const room = await db.collection('rooms').doc(this.$route.params.code).get();
+    // TODO: uncomment this validation
+    // const room = await db.collection('rooms').doc(this.$route.params.code).get();
 
-		// if (!room.exists) {
-		// 	this.error = 'This room does not exist!';
-		// 	return;
-		// }
+    // if (!room.exists) {
+    // 	this.error = 'This room does not exist!';
+    // 	return;
+    // }
 
-		this.code = this.$route.params.code;
+    this.code = this.$route.params.code;
 
-		this.$bind('messages', db.collection('rooms').doc(this.code).collection('messages').orderBy('created_at', 'asc'));
-	},
+		// Bind messages documents to messages state
+    this.$bind('messages', db.collection('rooms').doc(this.code).collection('messages').orderBy('created_at', 'asc')
+    );
+  },
 
-	methods: {
-		async deleteRoom() {
-			await db.collection('rooms').doc(this.code).delete();
-			router.push('/');
-		},
+  watch: {
+    messages() {
+      this.scrollToBottom();
+    },
+  },
 
-		async sendMessage() {
-			const text = this.textInput;
-			this.textInput = '';
+  methods: {
+    scrollToBottom() {
+			// Scroll to the bottom of the messages box
+			// TODO: Fix not scrolling to bottom sometimes (on message received)
+      let messagesBox = document.getElementById('messagesBox');
+      messagesBox.scrollTop = messagesBox.scrollHeight;
+    },
 
-			await db.collection('rooms').doc(this.code).collection('messages').doc().set({
-				text: text,
-				sender: this.userId,
-				created_at: firebase.firestore.FieldValue.serverTimestamp()
-			});
-		},
-	},
-}
+    async deleteRoom() {
+      await db.collection('rooms').doc(this.code).delete();
+      router.push('/');
+    },
+
+    async sendMessage() {
+      const text = this.textInput;
+      if (text.trim() == '') return;
+
+      this.textInput = '';
+
+      // Create a new message document
+      const newDoc = await db.collection('rooms').doc(this.code).collection('messages').doc();
+      const newDocRef = await newDoc.get();
+
+      // Set the fields in the message document
+      newDoc.set({
+        id: newDocRef.id,
+        text: text,
+        sender: this.userId,
+        created_at: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .chatbox {
-	background: #fff;
-	border-radius: 5px;
+  background: #fff;
+  border-radius: 5px;
 
-	.messages {
-		min-height: 60vh;		
+  .messages {
+    height: 60vh;
+    padding: 1em;
+    overflow: auto;
 
-		.bubble {
-			position: relative;
-			background: #4849A1;
-			color: #fff;
-			padding: 0.6rem;
-			border-bottom: 2px solid #eee;
-			border-radius: .4em;
-			-webkit-box-shadow: 5px 5px 9px -6px rgba(112,112,112,0.3); 
-			box-shadow: 5px 5px 9px -6px rgba(112,112,112,0.3);
-			margin-bottom: 10px;
-			display: inline-block;
-			max-width: 600px;
-			float: left;
-			clear: both;
-		}
+    .bubble {
+      max-width: 45%;
+      border-radius: 10px;
+      padding: 0.5em;
+      background: #407fff;
+      color: #fff;
+      margin-bottom: 0.5em;
+      float: left;
+      clear: both;
+    }
 
-		.received {
-			float: right !important;
-			background:#777 !important;
-		}
-	}
+    .received {
+      background: #f1f0f0;
+      color: #000;
+      float: right;
+    }
+  }
+}
+
+#messagesBox * {
+  overflow-anchor: none;
+}
+
+#anchor {
+  overflow-anchor: auto;
+  height: 1px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,52 @@
 <template>
-  <div>
-    <div class="columns">
-      <div class="column">
-        <h1 class="title">Welcome to chatbin!</h1>
-        <p>Your code is: <span class="has-text-weight-bold">{{ hostCode }}</span></p>
-        <div class="field">
-          <a class="button is-primary" @click="hostRoom()">Host a room</a>
-        </div>
-        <hr>
-        <p>Got someone's code? Join instead:</p>
-        <div class="field">
-          <input type="text" class="input" id="joinCode" v-model="joinCode" placeholder="Enter a code...">
-        </div>
-        <div class="field">
-          <a class="button is-primary" @click="joinRoom()">Join a room</a>
-        </div>
+  <div class="columns is-centered">
+    <div class="column is-half">
+      <h1 class="title">Welcome to chatbin!</h1>
+      <p>
+        Your code is: <span class="has-text-weight-bold">{{ hostCode }}</span>
+      </p>
+      <div class="field">
+        <a class="button is-primary" @click="hostRoom()">Host a room</a>
+      </div>
+      <hr />
+      <p>Got someone's code? Join instead:</p>
+      <div class="field">
+        <input
+          type="text"
+          class="input"
+          id="joinCode"
+          v-model="joinCode"
+          placeholder="Enter a code..."
+        />
+      </div>
+      <div class="field">
+        <a class="button is-primary" @click="joinRoom()">Join a room</a>
+      </div>
 
-        <div class="notification is-danger is-light"
-          v-for="error in errors" v-bind:key="error">
-          {{ error }}
-        </div>
+      <div
+        class="notification is-danger is-light"
+        v-for="error in errors"
+        v-bind:key="error"
+      >
+        {{ error }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import router from '@/router';
-import store from '@/store';
-import firebase from '@/firebase';
-import db from '@/db';
+import router from "@/router";
+import store from "@/store";
+//import firebase from "@/firebase";
+import db from "@/db";
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
       errors: [],
-      joinCode: ''
-    }
+      joinCode: "",
+    };
   },
 
   async created() {
@@ -53,31 +62,36 @@ export default {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
       return text;
-    }
+    },
   },
 
   methods: {
     hostRoom() {
-      db.collection('rooms').doc(this.hostCode).set({});
-      router.push('/chat/' + this.hostCode);
+      db.collection("rooms")
+        .doc(this.hostCode)
+        .set({});
+      router.push("/chat/" + this.hostCode);
     },
 
     async joinRoom() {
-      if (this.joinCode.trim() == '') return;
+      if (this.joinCode.trim() == "") return;
 
-      const room = await db.collection('rooms').doc(this.joinCode).get();
+      const room = await db
+        .collection("rooms")
+        .doc(this.joinCode)
+        .get();
       if (room.exists) {
-        router.push('/chat/' + this.joinCode);
+        router.push("/chat/" + this.joinCode);
       } else {
-        this.errors.push('Room code does not exist.');
+        this.errors.push("Room code does not exist.");
       }
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 #joinCode {
-  max-width: 150px; 
+  max-width: 150px;
 }
 </style>
